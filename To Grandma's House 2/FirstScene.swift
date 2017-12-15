42fc86e4586429662fd25b8aa3409f8557ee0ee3
noahmcmahon <noah.mcmahon@yahoo.com>
2017-12-15 10:30:56 -0500

    Changed screen size

---
+++ To Grandma's House 2/FirstScene.swift
@@ -0,0 +1,18 @@
+//
+//  FirstScene.swift
+//  To Grandma's House 2
+//
+//  Created by Noah McMahon on 12/15/17.
+//  Copyright © 2017 Hunter. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class FirstScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
