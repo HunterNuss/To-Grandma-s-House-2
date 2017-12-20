35fbbf723349dcc624a46ea422a580ae5b93f78a
HunterNuss <hjn6100@gmail.com>
2017-12-20 10:34:25 -0500

    Moved 2

--- To Grandma's House 2/GameScene.swift
+++ To Grandma's House 2/GameScene.swift
@@ -20,21 +20,19 @@ class GameScene: SKScene {
     override func sceneDidLoad() {
         littleRed = childNode(withName: "littleRed") as! SKSpriteNode
         littleRed.physicsBody?.categoryBitMask = PhysicsCategory.littleRed
-        print("Works")
     }
     
-    func touchR() {
+    func touchRed() {
         isFingerOnRed = true
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        touchR()
+        touchRed()
         self.isUserInteractionEnabled = true
         let touch = touches.first
         let touchLocation = touch!.location(in: self)
         if let body = physicsWorld.body(at: touchLocation) {
             if body.node?.name == "littleRed" {
-                print("Red")
                 isFingerOnRed = true
             }
         }
@@ -42,7 +40,6 @@ class GameScene: SKScene {
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if isFingerOnRed == true {
-            print("Moved")
             let touch = touches.first
             let touchLocation = touch!.location(in: self)
             littleRed.position = CGPoint(x: touchLocation.x, y: littleRed.position.y)
@@ -51,7 +48,6 @@ class GameScene: SKScene {
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         isFingerOnRed = false
-        touchR()
     }
 }
 
