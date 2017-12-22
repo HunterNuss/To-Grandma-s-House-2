079f838496183d18eba441c892940cfff3eac1af
HunterNuss <hjn6100@gmail.com>
2017-12-22 10:33:58 -0500

    FIXED EVERYTHING

--- To Grandma's House 2/FirstScene.swift
+++ To Grandma's House 2/FirstScene.swift
@@ -12,45 +12,27 @@ import GameplayKit
 
 class FirstScene: SKScene {
 
-    var nameLabel = SKLabelNode()
-    var button = UIButton()
-    var backgroundImage = UIImageView()
-    
+let start = SKLabelNode(fontNamed: "Arial")
     
     override func didMove(to view: SKView) {
-        background()
-        welcomeLabel()
-        transitionButton()
+        start.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        start.numberOfLines = 2
+        start.text = "Game Over\nClick Anywhere To Play Again"
+        start.fontSize = 40
+        start.fontColor = UIColor.white
+        addChild(start)
     }
     
-    func welcomeLabel() {
-        nameLabel = SKLabelNode(fontNamed: "Arial")
-        nameLabel.text = "To Grandma's House"
-        nameLabel.fontSize = 50
-        nameLabel.fontColor = UIColor.red
-        nameLabel.position = CGPoint(x: frame.width / 2, y: 50)
-        addChild(nameLabel)
-    }
     
-    func transitionButton() {
-        button = UIButton()
-        button.setTitle("Play", for: .normal)
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let gameScene = GameScene(fileNamed: "GameScene")
+        gameScene?.scaleMode = .aspectFill
+        gameScene?.size = self.size
+        let reveal = SKTransition.doorsCloseHorizontal(withDuration: 1)
+        view?.presentScene(gameScene!, transition: reveal)
         
-        let start = GameScene(size: self.size)
-       // let start = GameScene(fileNamed: "GameScene")
-        let reveal = SKTransition.flipVertical(withDuration: 1)
-        view?.presentScene(start, transition: reveal)
         
-        //                scene.scaleMode = .aspectFill
-
-    }
-    
-    
-    func background() {
-        backgroundImage.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        backgroundImage.image = #imageLiteral(resourceName: "Little Red Background")
-        view?.addSubview(backgroundImage)
-            
     }
     
 }
